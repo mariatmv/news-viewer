8d9cf1c2b6c74691cfa3a821d366252ca7ad406b
Dimitar Zhilanov <dimitar.zhilanov@kindredgroup.com>
2023-03-26 21:05:16 +0300

    added counter for multiple tabs

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 document.onreadystatechange = function () {
-    if (document.readyState == "interactive") {   
+    if (document.readyState == "interactive") { 
+        timestamp = Date.now()  
         document.querySelector('.action-btn').addEventListener("click", sendViewsToArticle)
+    
+        // if (localStorage.getItem("viewsCounter") == 'visible') {
+        //     document.querySelector('#viewsCounter').removeAttribute('hidden')
+        // }
     }
 }
 
@@ -12,16 +17,27 @@ async function sendViewsToArticle(event) {
     let viewsTimeWindowInSeconds = viewsTimeWindow * 60
     let waitPerRequest = viewsTimeWindowInSeconds / viewsCount 
 
-    document.querySelector('#articleUrl').value = ''
-    document.querySelector('#viewsCount').value = ''
-    document.querySelector('#viewsTimeWindow').value = ''
+    // document.querySelector('#articleUrl').value = ''
+    // document.querySelector('#viewsCount').value = ''
+    // document.querySelector('#viewsTimeWindow').value = ''
 
-    alert('Изпратено!')
+    // alert('Изпратено!')
+    let viewsCounter = document.querySelector('#viewsCounter')
+    viewsCounter.removeAttribute('hidden')
+    // localStorage.setItem(`${timestamp}_viewsCounter`, 'visible');
 
     for(let i = 1; i <= viewsCount; i++){
+        localStorage.setItem(`${timestamp}_viewsCount`, i);
         await fetchArticle(articleUrl, i, waitPerRequest)
     }
+
+    localStorage.setItem(`${timestamp}_viewsCount`, 0);
 }
+
+setInterval(function() {
+    let viewsCounter = document.querySelector('#viewsCounter h4')
+    viewsCounter.innerHTML = localStorage.getItem(`${timestamp}_viewsCount`)
+}, 2000)
 
 async function fetchArticle(articleUrl, i, waitPerRequest) {
     fetch(articleUrl, {
